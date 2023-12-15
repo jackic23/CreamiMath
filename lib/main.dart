e40c6e9ce8ce89abe075af561687bb2bab3cba0e
jackic23 <jackic23@gmail.com>
2023-12-15 04:26:35 +0000

    project

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CreamiMath());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CreamiMath extends StatelessWidget {
+  const CreamiMath({super.key});
 
   // This widget is the root of your application.
   @override
